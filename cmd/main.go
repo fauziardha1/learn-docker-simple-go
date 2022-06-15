e18b25032dc58e510124f1a1695032b18909e525
fauziardha1 <fauziardasaputra@gmail.com>
2022-06-15 23:50:32 +0700

    add env variable to container

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,21 @@
 package main
 
-
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// get port from environment variable
+	name := os.Getenv("NAME")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		// get value from environment variable
+		fmt.Fprintf(w, "Hello, %s!", name)
 	})
 
 	http.ListenAndServe(":8080", nil)
-	
+
 }
